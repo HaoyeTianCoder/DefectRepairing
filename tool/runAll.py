1793452ef74f222cf128a1ad737030944267457d
SophieHYe <he_ye_90s@hotmail.com>
2021-06-27 14:41:23 -0700

    fix

--- tool/runAll.py
+++ tool/runAll.py
@@ -6,13 +6,13 @@ if __name__ == '__main__':
    currentpath=os.path.dirname(os.path.realpath(__file__))
    r = re.compile("([a-zA-Z]+)([0-9]+)")
    for f in listdirs:
-        pattern = 'patch*Math*'
+        pattern = 'patch*rrect'
         if fnmatch.fnmatch(f, pattern):
            print(f) 
            with open(currentpath+'/patches/'+f) as file:
                arraynames=f.split("-")  
                project= arraynames[1]
-               bug= arraynames[2]
+               bug= arraynames[2].split("_")[0]
                os.chdir('source') 
                os.system('python3 run.py '+project+'  '+bug+'  '+f) 
                os.chdir('..') 
