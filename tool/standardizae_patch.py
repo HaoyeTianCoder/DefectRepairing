b8c0c3121a7ac9781746c748c1bbafa5376ee558
haoye tian <363730631@qq.com>
2021-06-30 12:01:28 +0200

    none

--- tool/standardizae_patch.py
+++ tool/standardizae_patch.py
@@ -1,6 +1,6 @@
 import os, fnmatch
 
-abpatch = ['AVATAR', 'DynaMoth', 'FixMiner', 'kPAR', 'TBar', 'Developer']
+abpatch = ['AVATAR', 'DynaMothTest', 'FixMiner', 'kPAR', 'TBar', 'Developer']
 
 def parse_abpatch(path, tool):
     new_path = os.path.join(path, tool)
@@ -103,7 +103,7 @@ def parse_kPAR(path, tool):
                     f.write(new_line)
 
 if __name__ == '__main__':
-    path = './available'
+    path = 'available'
     tools = os.listdir(path)
     for tool in tools:
         if tool in abpatch:
