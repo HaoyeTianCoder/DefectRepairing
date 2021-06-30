5634a3d1a79dc0dc1bd97384bddd1526e0b34e0f
haoye tian <363730631@qq.com>
2021-06-30 09:53:46 +0200

    skip existed result

--- tool/runAll.py
+++ tool/runAll.py
@@ -2,13 +2,22 @@
 import sys, os, subprocess,fnmatch,csv,re
 
 if __name__ == '__main__':
+   exist_result = set()
+   csv_reader = csv.reader(open('./source/RESULT.csv'))
+   for line in csv_reader:
+       patch_name = line[0]
+       exist_result.add(patch_name)
+
    listdirs = os.listdir('./patches')
    currentpath=os.path.dirname(os.path.realpath(__file__))
    r = re.compile("([a-zA-Z]+)([0-9]+)")
    for f in listdirs:
         pattern = 'patch*rrect'
         if fnmatch.fnmatch(f, pattern):
-           print(f) 
+           print(f)
+           if f in exist_result:
+               print("{}'s result exists".format(f))
+               continue
            with open(currentpath+'/patches/'+f) as file:
                arraynames=f.split("-")  
                project= arraynames[1]
