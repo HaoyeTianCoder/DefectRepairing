1a36325dd43281d6218843b4f9dd4328304554e0
haoye tian <363730631@qq.com>
2021-07-01 14:30:37 +0200

    parallelize

--- tool/source/runAll.py
+++ tool/source/runAll.py
@@ -2,7 +2,17 @@
 import sys, os, subprocess,fnmatch,csv,re
 import time
 from run import run
+import multiprocessing
 # sys.path.append('')
+
+cores = multiprocessing.cpu_count()
+print ("cores: ", cores)
+pool = multiprocessing.Pool(processes=cores)
+
+def runMain(para_list):
+    project, bug, f = para_list[0], para_list[1], para_list[2]
+    run(project, bug, f)
+
 
 if __name__ == '__main__':
     exist_result = set()
@@ -12,9 +22,10 @@ if __name__ == '__main__':
            patch_name = line[0]
            exist_result.add(patch_name)
 
-    listdirs = os.listdir('../patches')
+    listdirs = os.listdir('./patches')
     currentpath=os.path.dirname(os.path.realpath(__file__))
     r = re.compile("([a-zA-Z]+)([0-9]+)")
+    cmd_list = []
     for f in listdirs:
         pattern = 'patch*rrect'
         if fnmatch.fnmatch(f, pattern):
@@ -29,7 +40,9 @@ if __name__ == '__main__':
 
                # os.chdir('')
                # os.system('python3 run.py '+project+'  '+bug+'  '+f)
-               run(project, bug, f)
+               cmd_list.append([project, bug, f])
                # os.chdir('../..')
+
+    pool.map(runMain, cmd_list)
 
 
