2c83423283f78332768f9bcfc31e360f46869856
haoyetiancoder <363730631@qq.com>
2022-03-10 15:59:27 +0100

    none

--- tool/source/run.py
+++ tool/source/run.py
@@ -97,7 +97,7 @@ def run(project,bugid,patch_no):
     gen_test_randoop(project,bugid)
 
     signal.signal(signal.SIGALRM, handler)
-    signal.alarm(3600)
+    signal.alarm(180)
     try:
         trace(project,bugid,patch_no)
     except Exception as e:
